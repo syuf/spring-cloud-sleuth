75cf6c46df3849a529c76ed5632b9e391e5de673
shiyuanfei <356418670@qq.com>
2018-06-28 18:01:07 +0800

    初始化项目

---
+++ spring-cloud-sleuth/src/main/java/com/zlpay/SpringCloudSleuthApplication.java
@@ -0,0 +1,12 @@
+package com.zlpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudSleuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudSleuthApplication.class, args);
+	}
+}
